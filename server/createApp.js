4de248c8eaefdcc97e3c5d8ff96c837472f418a3
Mohammad Taleb <mohammadtmohsen@gmail.com>
2025-08-08 04:41:52 +0300

    fix: simplify CORS configuration in createApp

--- server/createApp.js
+++ server/createApp.js
@@ -3,12 +3,11 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import passport from 'passport';
 import routes from './routes/index.js';
-import corsOptions from './config/corsOptions.js';
 import './strategies/local-strategy.js';
 
 export function createApp() {
   const app = express();
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(express.json());
   app.use(cookieParser('cookieParser'));
   app.use(passport.initialize());
